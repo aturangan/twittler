c4d605dee28a30f86df62d04fbe56d99f634b7c9
Alana <alana.turangan@gmail.com>
2017-03-19 18:12:01 -0700

    files added  Please enter the commit message for your changes. Lines starting

---
+++ index_files/twittler.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+
+
+
+
+
+    function listTweets(array) {
+        for (var i = 0; i < array.length; i++) {
+            return array[i];
+        }
+    }
+
+    listTweets(streams.home);
+}
