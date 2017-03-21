a4546dc60965f8c206e0540f8a4a01d155da641f
Alana <alana.turangan@gmail.com>
2017-03-20 19:56:56 -0700

    index files

--- index_files/twittler.js
+++ index_files/twittler.js
@@ -1,15 +1,42 @@
+
 $(document).ready(function() {
 
-
-
-
-
-
-    function listTweets(array) {
-        for (var i = 0; i < array.length; i++) {
-            return array[i];
-        }
+    for (var user in streams.users) {
+      $('#users').append('<li class="link" data-user='+user+'>@' + user + '</li>');
     }
 
-    listTweets(streams.home);
-}
+    var tweetList = streams.home;
+
+    function displayTweets(begin, end, tweetList) {
+      var i = end;
+      while (begin <= end) {
+
+        var tweet = tweetList[i];
+        var user = tweetList[i].user;
+        var date = tweetList[i].created_at;
+
+        var $tweet = $('<div class="tweet"><div class="title">' + date + user + '</div>' + tweet.message + '</div>');
+
+        $tweet.prependTo($('#tweets').fadeIn());
+
+        begin += 1;
+      }
+    }
+
+    displayTweets(0, (streams.home.length - 1), tweetList);
+
+
+    $('#users').on('click', '.link', function(element) {
+      element.preventDefault();
+
+      $('#tweets').empty();
+
+      var name = $(this).attr('data-user');
+      var timeline = streams.users[name].length - 1;
+
+      while (timeline >= 0) {
+        displayTweets(0, timeline, streams.users[name]);
+        timeline -= 1;
+      }
+    });
+});
